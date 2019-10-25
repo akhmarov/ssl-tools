d1af98ef53bbed3409d37d670b9666c817f8f339
Vladimir Akhmarov <hyperwyrm@hypermac.msk.i-teco.ru>
2019-10-25 20:51:55 +0300

    Updated WLC file

--- tls-cisco-wlc.cmd
+++ tls-cisco-wlc.cmd
@@ -3,7 +3,7 @@
 rem #
 rem # Name: tls-cisco-wlc.cmd
 rem #
-rem # Date: March 2017
+rem # Date: October 2019
 rem #
 rem # Author: Vladimir Akhmarov
 rem #
@@ -19,6 +19,7 @@ rem #        6. Select checkbox Download SSL certificate and fill necessary fiel
 rem #        7. Press Apply button
 rem #        8. Navigate to COMMANDS -> Reboot
 rem #        9. Press Save and Reboot button
+rem #        10. Repeat steps 1-9 on second controller from CLI
 rem #
 
 set WORK_DIR=data\cisco-wlc
@@ -45,16 +46,6 @@ cls
 
 set /p PFX_PASS=Enter PFX password:
 
-rem Extracting client certificate to %WORK_DIR%\mgmt-cert-tmp.pem
-%OPENSSL% pkcs12 -in %CERT_PFX% -out %WORK_DIR%\mgmt-cert-tmp.pem -passin pass:%PFX_PASS% -nokeys -clcerts
-rem Remove bag attributes from certificate file
-%OPENSSL% x509 -in %WORK_DIR%\mgmt-cert-tmp.pem -out %WORK_DIR%\mgmt-cert.pem
-rem Extracting private key to %WORK_DIR%\mgmt-key.pem
-%OPENSSL% pkcs12 -in %CERT_PFX% -out %WORK_DIR%\mgmt-key.pem -passin pass:%PFX_PASS% -nocerts -passout pass:%PEM_PASS%
-rem Extracting PKCS12 archive to %WORK_DIR%\mgmt-arch.pem
-%OPENSSL% pkcs12 -in %WORK_DIR%\mgmt-cert.pem -out %WORK_DIR%\mgmt-arch.p12 -export -inkey %WORK_DIR%\mgmt-key.pem -passin pass:%PEM_PASS% -passout pass:%PEM_PASS% -clcerts
-%OPENSSL% pkcs12 -in %WORK_DIR%\mgmt-arch.p12 -out %WORK_DIR%\mgmt-arch.pem -passin pass:%PEM_PASS% -passout pass:%PEM_PASS%
-
 rem Extracting client certificate to %WORK_DIR%\wa-cert-cl-tmp.pem
 %OPENSSL% pkcs12 -in %CERT_PFX% -out %WORK_DIR%\wa-cert-cl-tmp.pem -passin pass:%PFX_PASS% -nokeys -clcerts
 rem Remove bag attributes from client, intermediate and root certificate files
@@ -69,8 +60,7 @@ rem Extracting PKCS12 archive to %WORK_DIR%\wa-arch.pem
 %OPENSSL% pkcs12 -in %WORK_DIR%\wa-cert.pem -out %WORK_DIR%\wa-arch.p12 -export -inkey %WORK_DIR%\wa-key.pem -passin pass:%PEM_PASS% -passout pass:%PEM_PASS% -clcerts
 %OPENSSL% pkcs12 -in %WORK_DIR%\wa-arch.p12 -out %WORK_DIR%\wa-arch.pem -passin pass:%PEM_PASS% -passout pass:%PEM_PASS%
 
-echo Archive (Management): %WORK_DIR%\mgmt-arch.pem (password: %PEM_PASS%)
-echo Archive (WebAuth): %WORK_DIR%\wa-arch.pem (password: %PEM_PASS%)
+echo Archive: %WORK_DIR%\wa-arch.pem (password: %PEM_PASS%)
 
 echo WARNING! All files will be deleted!
 
